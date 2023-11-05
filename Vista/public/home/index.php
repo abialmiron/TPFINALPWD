1cdc570a1115fd4973a60d061d8f4c1d6cdbd982
LZDEV <81957303+Lestradez@users.noreply.github.com>
2023-11-05 15:55:42 -0300

    feat: :art: estructura

---
+++ Vista/public/home/index.php
@@ -0,0 +1,9 @@
+<main>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>Home de la vista pública</h1>
+            </div>
+        </div>
+    </div>
+</main>
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php 
+header('Location: Vista/public/home/index.php');
+?>
