676e68230a64bb7d14d40b3e53a3f0a23e07c7e3
LZDEV <81957303+Lestradez@users.noreply.github.com>
2023-11-06 02:17:07 -0300

    feat: :art: video mas parallax
    prueba de video background más parallax

--- Vista/public/index.php
+++ Vista/public/index.php
@@ -1,11 +1,35 @@
 <?php
 include_once("../../configuracion.php");
+include_once("../estructura/header.php");
 ?>
 <main>
+    <div class="paravideo">
+        <video autoplay muted loop>
+            <source src="../images/nursery_-_567-converted.mp4" type="video/mp4">
+        </video>
+        <h2>Video Background</h2>
+    </div>
     <div class="container">
         <div class="row">
             <div class="col-12">
                 <h1>Home de la vista pública</h1>
+                <div class="paracontent">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-12">
+                                <h1>Home de la vista pública</h1>
+                                <p>lorem ipsum dolor sit amet consectetur a dipisicing</p>
+                                <p>lorem ipsum dolor sit amet consectetur a dipisicing</p>
+                                <p>lorem ipsum dolor sit amet consectetur a dipisicing</p>
+                                <p>lorem ipsum dolor sit amet consectetur a dipisicing</p>
+                                <p>lorem ipsum dolor sit amet consectetur a dipisicing</p>
+                                <p>lorem ipsum dolor sit amet consectetur a dipisicing</p>
+                                <p>lorem ipsum dolor sit amet consectetur a dipisicing</p>
+                                <p>lorem ipsum dolor sit amet consectetur a dipisicing</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
