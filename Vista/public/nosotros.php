2ea2edbf2b4365d203aaab84285907c03ea67451
LZDEV <81957303+Lestradez@users.noreply.github.com>
2023-11-13 14:10:30 -0300

    feat: :art: añadir página nosotros

---
+++ Vista/public/nosotros.php
@@ -0,0 +1,69 @@
+<?php
+include_once("../estructura/head.php");
+?>
+<main class="row justify-content-center align-items-center m-auto">
+
+<div class="container">
+    <div class="row mb-4 mb-lg-5">
+        <div class="col-md-8 col-xl-6 text-center mx-auto">
+            <h2 class="fw-bold">HAY EQUIPO</h2>
+            <p class="text-muted">Grupo 9.2 PWD-2023 </p>
+        </div>
+    </div>
+    <div class="row row-cols-2 row-cols-md-3 mx-auto" style="">
+        <div class="col mb-4">
+          <div class="text-center"><img class="rounded-circle mb-3 fit-cover" width="200" height="200" src="../images/abi-git.jpg" />
+            <h5 class="fw-bold mb-0"><strong>Abigail Almirón</strong></h5>
+            <p class="text-muted mb-2">
+              <i class="bi bi-mortarboard-fill fs-5"></i> <span>Legajo: FAI-3147</span>
+            </p>
+            <p class="text-muted mb-2">
+              <i class="bi bi-envelope-at-fill fs-5 "></i> <span>Email: abigail.almiron@est.fi.uncoma.edu.ar</span>
+            </p>
+            <p class="text-muted mb-2">
+              <a href="https://github.com/abialmiron/" role="button" class="text-reset text fs-5 text-decoration-none ">
+                <i class="bi bi-github" ></i> <span>AbiAlmiron</span>
+              </a>
+            </p>
+          </div>
+        </div>
+        <div class="col mb-4">
+            <div class="text-center"><img class="rounded-circle mb-3 fit-cover" width="200" height="200" src="../images/agus-git.png" />
+                <h5 class="fw-bold mb-0"><strong>Agustina Flores</strong></h5>
+                <p class="text-muted mb-2">
+                        <i class="bi bi-mortarboard-fill fs-5"></i> <span>Legajo: FAI-3547</span>
+                      </p>
+                      <p class="text-muted mb-2">
+                        <i class="bi bi-envelope-at-fill fs-5 "></i> <span>Email: ailen.flores@est.fi.uncoma.edu.ar</span>
+                      </p>
+                      <p class="text-muted mb-2">
+                        <a href="https://github.com/agusflores1/" role="button" class="text-reset text fs-5 text-decoration-none ">
+                          <i class="bi bi-github" ></i> <span>AgusFlores</span>
+                        </a>
+                      </p>
+            </div>
+        </div>
+        <div class="col mb-4">
+            <div class="text-center"><img class="rounded-circle mb-3 fit-cover" width="200" height="200" src="../images/omar-git.png" />
+                <h5 class="fw-bold mb-0"><strong>Omar Lestrade</strong></h5>
+                <p class="text-muted mb-2">
+                        <i class="bi bi-mortarboard-fill fs-5"></i> <span>Legajo: FAEA-775</span>
+                      </p>
+                      <p class="text-muted mb-2">
+                        <i class="bi bi-envelope-at-fill fs-5 "></i> <span>Email: omar.lestrade@est.fi.uncoma.edu.ar</span>
+                      </p>
+                      <p class="text-muted mb-2">
+                        <a href="https://github.com/Lestradez/" role="button" class="text-reset text fs-5 text-decoration-none ">
+                          <i class="bi bi-github" ></i> <span>Lestradez</span>
+                        </a>
+                      </p>
+            </div>
+        </div>
+    </div>
+</div>
+
+</main>
+
+<?php
+include_once("../estructura/footer.php");
+?>
