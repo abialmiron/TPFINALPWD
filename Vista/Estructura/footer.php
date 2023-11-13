6b5adfc187c6e2525573c09b88816b8155ae88a3
LZDEV <81957303+Lestradez@users.noreply.github.com>
2023-11-13 00:26:51 -0300

    refactor: :bug: actualizar jquery
    se actualiza jquery porque la version slim no soporta ajax

--- Vista/Estructura/footer.php
+++ Vista/Estructura/footer.php
@@ -5,7 +5,7 @@
 </footer>
 </body>
 </html>
-<script src="<?php echo BASE_URL ?>Vista/js/jquery/jquery-3.5.1.slim.min.js"></script>
+<script src="<?php echo BASE_URL ?>Vista/js/jquery/jquery-3.7.1.min.js"></script>
 <script src="<?php echo BASE_URL ?>Vista/js/bootstrap.bundle.min.js"></script>
 <script src="<?php echo BASE_URL ?>Vista/js/validator.js"></script>
 <script src="<?php echo BASE_URL ?>Vista/js/menu.js"></script>
