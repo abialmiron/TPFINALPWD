a88ef50ec704521526e9f3cb92f9c9e1c79b1406
abialmiron <abialmiron@gmail.com>
2023-11-14 19:24:50 -0300

    cambios en usuario.php

--- Vista/private/administrador/usuarios.php
+++ Vista/private/administrador/usuarios.php
@@ -1,5 +1,5 @@
 <?php
-include_once("../../configuracion.php");
+include_once("../../estructura/head.php");
 $objUsuarios = new AbmUsuario();
 $listaUsuario = $objUsuarios->buscar(null);
 $objRol = new AbmRol();
