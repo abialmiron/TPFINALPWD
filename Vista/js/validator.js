6a232f005fb5c381102e7c7adb63b6058933bd7b
LZDEV <81957303+Lestradez@users.noreply.github.com>
2023-11-15 04:12:02 -0300

    refactor: :art: mejorar validacion en formulario login

--- Vista/js/validator.js
+++ Vista/js/validator.js
@@ -1,64 +1,37 @@
-$(document).ready(function () {
-  // Controlador de eventos 'input' para el campo usuario
-  $("#usuario").on("input", function () {
-      var fieldValue = $(this).val().trim();
-      if (fieldValue === "" || !/^[a-zA-Z]+$/.test(fieldValue)) {
-          $(this).removeClass("is-invalid");
-      } else {
-          $(this).addClass("is-invalid");
-      }
-  });
+$(document).ready(function() {
+  // Expresiones regulares para la validación
+  var usuarioRegex = /^[^\s!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]{4,}$/;
+  var passwordRegex = /^(?=.*\d)(?=.*[A-Z])(?=.*[\W_])[^ ]{8,}$/;
 
-  // Controlador de eventos 'input' para el campo clave
-  $("#password").on("input", function () {
-      var claveValue = $(this).val().trim();
-      var usuarioValue = $("#usuario").val().trim();
+  // Función para validar el campo de usuario
+  function validarUsuario() {
+    var usuario = $('#usuario').val();
+    if (usuarioRegex.test(usuario)) {
+      console.log('Usuario válido');
+      $(this).removeClass("is-invalid");
+    } else {
+      console.log('Usuario inválido');
+      $(this).addClass("is-invalid");
+    }
+  }
 
-      if (
-          claveValue.length > 7 &&
-          claveValue !== usuarioValue &&
-          /^[0-9]+$/.test(claveValue)
-      ) {
-          $(this).removeClass("is-invalid");
-      } else {
-          $(this).addClass("is-invalid");
-      }
-  });
+  // Función para validar el campo de contraseña
+  function validarPassword() {
+    var password = $('#password').val();
+    if (passwordRegex.test(password)) {
+      console.log('Contraseña válida');
+      $(this).removeClass("is-invalid");
+    } else {
+      console.log('Contraseña inválida');
+      $(this).addClass("is-invalid");
+    }
+  }
+
+  // Validación en tiempo real al escribir en los campos
+  $('#usuario').on('input', validarUsuario);
+  $('#password').on('input', validarPassword);
 });
 
-$(document).ready(function () {
-    // Controlador de envío del formulario
-    $("#formLogin").submit(function (e) {
-      var claveValue = $("#password").val().trim();
-      var usuarioValue = $("#usuario").val().trim();
-      var isValid = true;
-  
-      // Validar campo de usuario
-      if (usuarioValue === "" || usuarioValue.length < 4 || !/^[a-zA-Z]+$/.test(usuarioValue)) {
-        $("#usuario").addClass("is-invalid");
-        isValid = false;
-      } else {
-        $("#usuario").removeClass("is-invalid");
-      }
-  
-      // Validar campo de contraseña
-      if (
-        claveValue === "" ||
-        claveValue === usuarioValue ||
-        claveValue.length < 8
-      ) {
-        $("#password").addClass("is-invalid");
-        isValid = false;
-      } else {
-        $("#password").removeClass("is-invalid");
-        
-      }
-  
-      if (!isValid) {
-        e.preventDefault(); // Evitar el envío del formulario si hay errores
-      }
-    });
-  });
   
 $(document).ready(function() {
     $("#form").submit(function(event) {
