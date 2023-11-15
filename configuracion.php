5010503bf17407b4892bdc87905b6b29ddd29d28
LZDEV <81957303+Lestradez@users.noreply.github.com>
2023-11-15 04:18:53 -0300

    feat: perodon pero toqué este archivo y van a tener que volver a configurar el directorio raíz

--- configuracion.php
+++ configuracion.php
@@ -10,7 +10,16 @@ $directorio = '/PWD2023/MI_GRUPO/tp-final/TPFINALPWD/'; // Escribir el directori
 define('ROOT_PATH', $_SERVER['DOCUMENT_ROOT'].$directorio);
 define('BASE_URL', 'http://'.$_SERVER['HTTP_HOST'].$directorio);
 define('STRUCTURE_PATH', ROOT_PATH.'Vista/Estructura/');
+define('CSS_PATH', BASE_URL.'Vista/css/');
+define('JS_PATH', BASE_URL.'Vista/js/');
+define('IMG_PATH', BASE_URL.'Vista/images/');
+define('FONT_PATH', BASE_URL.'Vista/fonts/');
+$GLOBALS['error'] = array(
+  1 => 'No posee permisos para acceder a esta página.',
+  2 => 'No se pudo realizar la acción solicitada.',
+  3 => 'Hubo algún error inténte nuevamente más tarde.',);
+
 $_SESSION['ROOT'] = ROOT_PATH;
 $ROOT = ROOT_PATH; // Agrega esta línea para definir la variable $ROOT
 include_once(ROOT_PATH.'util/funciones.php');
-// include_once(ROOT_PATH.'Vista/Estructura/head.php');
+sesionActiva();
